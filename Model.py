cd532888ed1dce691fece409ec99160577f7b033
deboradum <pvwpvw@protonmail.com>
2024-12-22 20:40:13 +0100

    fm part

--- Model.py
+++ Model.py
@@ -1,16 +1,13 @@
-import tinygrad
 from tinygrad import Tensor, nn
 
 
-class FactorizationMachine:
-    def __init__(self):
-        return
-
-    def __call__(self, x):
-        return
-
-
 # TODO: Experiment with different initializations
+# TODO: after that, a dense real-value feature vector is gener-
+#       ated, which is finally fed into the sigmoid function for CTR
+#       prediction: yDN N = σ(W |H|+1 · aH + b|H|+1), where |H|
+#       is the number of hidden layers.
+# TODO: Add dropout + batch/ layer norm
+# https://arxiv.org/abs/1703.04247
 class DeepNet:
     def __init__(self, input_dim, num_layers, hidden_dim):
         self.l1 = nn.Linear(input_dim, hidden_dim)
@@ -32,10 +29,15 @@ class DeepFM:
         self.m = len(feature_sizes)
         self.feature_sizes = feature_sizes
 
-        self.o1_embeddings = [nn.Embedding(field_size, 1) for field_size in self.feature_sizes]
-        self.o2_embeddings = [nn.Embedding(field_size, k) for field_size in self.feature_sizes]
+        self.o1_embeddings = [
+            nn.Embedding(field_size, 1) for field_size in self.feature_sizes
+        ]
+        self.o2_embeddings = [
+            nn.Embedding(field_size, k) for field_size in self.feature_sizes
+        ]
 
-        self.fm = FactorizationMachine()
+        self.bias = Tensor.rand(1)
+
         self.deep = DeepNet(
             input_dim=self.m * k,
             num_layers=6,
@@ -44,9 +46,17 @@ class DeepFM:
 
     def __call__(self, x):
         # convert sparse x to dense x by concatenating feature embeddings
-        prev_i = 0
+        dense_x_o1_list = [
+            embed_layer(
+                x[
+                    :, sum(self.feature_sizes[:i]) : sum(self.feature_sizes[: i + 1])
+                ].argmax(axis=1)
+            )
+            for i, embed_layer in enumerate(self.o1_embeddings)
+        ]
+        dense_x_o1 = Tensor.cat(*dense_x_o1_list, dim=1)
 
-        dense_x_o2 = [
+        dense_x_o2_list = [
             embed_layer(
                 x[
                     :, sum(self.feature_sizes[:i]) : sum(self.feature_sizes[: i + 1])
@@ -54,12 +64,25 @@ class DeepFM:
             )
             for i, embed_layer in enumerate(self.o2_embeddings)
         ]
-        dense_x_o2 = Tensor.cat(*dense_x_o2, dim=1)
+        dense_x_o2 = Tensor.cat(*dense_x_o2_list, dim=1)
 
-        y_fm = self.fm(dense_x_o2)
-        y_dnn = self.deep(dense_x_o2)
+        y_fm = self.fm(dense_x_o1, dense_x_o2_list, dense_x_o2)
+        y_dnn = self.deep(dense_x_o2).sum(axis=1)
 
-        return (y_fm + y_dnn).sigmoid()
+        return y_fm + y_dnn + self.bias
+
+    # From # https://www.ismll.uni-hildesheim.de/pub/pdfs/Rendle2010FM.pdf:
+    # 0.5 * \sum^{k}_{f=1}( (\sum^{n}_{i=1} v_{i, f} x_{i} )^{2} - \sum^{n}_{i=1} v^{2}_{i, f} x^{2}_{i} )
+    # t1 = (\sum^{n}_{i=1} v_{i, f} x_{i} )^{2}
+    # t2 = \sum^{n}_{i=1} v^{2}_{i, f} x^{2}_{i}
+    def fm(self, fm_o1, dense_x_o2_list, dense_x_o2):
+        # equivalent to (\sum^{n}_{i=1} v_{i, f} x_{i} )^{2}, because x is one hot encoded
+        t1 = sum(dense_x_o2_list).square()
+        t2 = sum([e*e for e in dense_x_o2_list])
+
+        fm_o2 = 0.5 * (t1 - t2).sum(axis=1)
+
+        return fm_o1.sum(axis=1) + fm_o2.sum(axis=1)
 
 
 d = DeepFM([3, 5, 2], 2)
